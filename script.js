55cef0431dc730a9019f2a8bfe323894f570145c
example <alisayar0107@gmail.com>
2021-01-08 22:58:08 +0300

    the slider moon icon added, writing is changed

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const textBox = document.getElementById('text-box');
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.remove('fa-sun');
+  toggleIcon.children[1].classList.add('fa-moon');
 }
+
+
 
 // Swtich theme dynamically
 function switchTheme(event){
