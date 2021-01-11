50e82f985d4c133e652ec50ee4cf3646d6a892b8
example <alisayar0107@gmail.com>
2021-01-11 18:51:51 +0300

    bugs are solved. everything is working, media queries will be added tomorrow.

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const LIGHT_THEME = 'light';
 
 // Dark or Light Images
 function imageMode(color) {
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
   image1.src = `img/undraw_feeling_proud_${color}.svg`
   image2.src = `img/undraw_conceptual_idea_${color}.svg`
   image3.src = `img/undraw_proud_coder_${color}.svg`
