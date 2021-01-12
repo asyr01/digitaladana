a982153b2edaefa3e7702f42c5cdf513fc9bd530
example <alisayar0107@gmail.com>
2021-01-12 17:09:31 +0300

    hamburger added for mobile

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
+const hamburger = document.getElementById('navClosed');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
@@ -20,6 +21,7 @@ function imageMode(color) {
 function toggleDarkLightMode(isDark) {
   nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' :  'rgb(255 255 255 / 50%)' ;
   textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)' ;
+  hamburger.classList.add = isDark ? 'filter-blue' : '';
   toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
   isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : 
    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
